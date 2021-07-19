8d8d930a10b665cb963087aadb3076354d280703
mdee <design4web2014@gmail.com>
2021-07-19 10:51:21 +0200

    Second challenge: balanced brackets

---
+++ src/balanced-bracket.js
@@ -0,0 +1,31 @@
+// Given a string containing brackets, determine if all brackets have a matching counterpart. If all brackets in the string form balanced pairs, return true. If not, return false
+
+const balancedBracket = str => {
+  let arr = str.split("");
+
+  let obj = {};
+
+  for (let el of arr) {
+    if (obj[el]) {
+      obj[el]++;
+    } else {
+      obj[el] = 1;
+    }
+  }
+
+  let objVals = Object.values(obj)
+   
+  let result = objVals.reduce((total, currAmo) => {
+    return total + currAmo
+  })
+
+  if(result % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// Test Data :
+console.log(balancedBracket("[()]{}[{({})()]()}"))
+console.log(balancedBracket("[)]{}[{(()}"))
